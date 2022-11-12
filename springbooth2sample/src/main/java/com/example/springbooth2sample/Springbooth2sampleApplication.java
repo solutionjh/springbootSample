19f8bd5835d0f6aaa9d3cf58636a022d8e4d4a74
solutionjh <solutionjh@nice.co.kr>
2022-11-12 20:22:15 +0900

    initial project

---
+++ springbooth2sample/src/main/java/com/example/springbooth2sample/Springbooth2sampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbooth2sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooth2sampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbooth2sampleApplication.class, args);
+	}
+
+}
---
+++ springbooth2sample/src/test/java/com/example/springbooth2sample/Springbooth2sampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbooth2sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbooth2sampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
